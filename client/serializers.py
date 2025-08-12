f0fd942645e635f3448eb8811cfadd0b74322ad7
David Gift <giftdavid189@gmail.com>
2025-08-12 22:48:29 +0100

    impliment the serializers

--- client/serializers.py
+++ client/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Client
+from account.serializers import CustomUserSerializer
 
 class ClientSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer(read_only=False)
     class Meta:
         model = Client
         fields = '__all__'
--- request/serializers.py
+++ request/serializers.py
@@ -1,18 +1,24 @@
 from rest_framework import serializers
 from . import models
+from client.serializers import ClientSerializer
+from staff.serializers import StaffSerializer
 
 class ServiceRequestSerializer(serializers.ModelSerializer):
+    client = ClientSerializer(many=True, read_only=True)
+    staff = StaffSerializer()
     class Meta:
         model = models.ServiceRequest
         fields = '__all__'
 
 class RequestNoteSerializer(serializers.ModelSerializer):
     request = ServiceRequestSerializer(many=True, read_only=True)
+    staff = StaffSerializer(many=True, read_only=True)
     class Meta:
         model = models.ServiceRequest
         fields = '__all__'
 
 class FeedbackSerializer(serializers.ModelSerializer):
+    request = ServiceRequestSerializer(many=True, read_only=True)
     class Meta:
         model = models.Feedback
         fields = '__all__'
