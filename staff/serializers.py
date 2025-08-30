3d88ab5947cbf8bdc9b2cf37e181d4920cb1fb49
David Gift <giftdavid189@gmail.com>
2025-08-30 15:26:19 +0100

    updated staff update feature

--- staff/serializers.py
+++ staff/serializers.py
@@ -15,16 +15,3 @@ class StaffSerializer(serializers.ModelSerializer):
         user_serializer.is_valid(raise_exception=True)
         user_instance = user_serializer.save()
         staff_instance = Staff.objects.create(user=user_instance, **validated_data)
-
-        return staff_instance
-    
-    # def update(self, instance, validated_data):
-    #     # Check if the username is being updated and if it's the same as the current one
-    #     if 'username' in validated_data and instance.username == validated_data['username']:
-    #         # If the username hasn't changed, remove it from the validated data
-    #         # to prevent the unique constraint from being checked.
-    #         del validated_data['username']
-
-    #     # Call the parent update method to handle the rest of the fields
-    #     return super().update(instance, validated_data)
-
